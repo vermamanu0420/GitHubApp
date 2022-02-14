9b415413073a4882982905013ed42d91c502609e
Manu Verma <vermamanu0420@gmail.com>
2022-02-14 00:13:59 -0800

    View added
    Fragments and Layout added

---
+++ app/src/main/java/com/example/githubapp/view/GitHubUserDetailFragment.java
@@ -0,0 +1,30 @@
+package com.example.githubapp.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.githubapp.R;
+
+public class GitHubUserDetailFragment extends Fragment {
+
+    public GitHubUserDetailFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_git_hub_user_detail, container, false);
+    }
+}
--- app/src/main/java/com/example/githubapp/view/MainActivity.java
+++ app/src/main/java/com/example/githubapp/view/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .setReorderingAllowed(true)
+                    .add(R.id.fragment_container_view, GitHubUserDetailFragment.class, null)
+                    .commit();
+        }
     }
 }
